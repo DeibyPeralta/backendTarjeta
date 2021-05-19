494d3b2bfd9841ccdf7d90cd9e84fa289b13cf6b
newSteven <deibysp39@gmail.com>
2021-05-19 14:01:30 -0500

    Agregar archivos de proyecto.

---
+++ modelo/TarjetaCredito.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backendTarjeta.modelo
+{
+    public class TarjetaCredito
+    {
+        [Key] // le digo a entity framework que id es la primary key
+        public int Id { get; set; }
+
+        [Required] // campos requeridos
+        public string titular { get; set; }
+        [Required]
+        public string numeroTarjeta { get; set; }
+        [Required]
+        public string fechaExpiracion { get; set; }
+        [Required]
+        public string cvv { get; set; }
+    }
+}
